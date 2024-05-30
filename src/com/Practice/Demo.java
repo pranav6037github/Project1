121d1210a377d9ab94348766fb072a8e73cd8b3c
Pranav Joshi <pranavjoshi6037@gmail.com>
2024-05-31 04:11:41 +0530

    My First Commit

---
+++ src/com/Practice/Demo.java
@@ -0,0 +1,25 @@
+package com.Practice;
+
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[]args) {
+		
+		Demo d = new Demo();
+		d.a1();
+		
+	}
+	
+	public void a1 () {
+		
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("Pranav");
+    list.add("Mahesh");
+    list.add("Prachi");
+    list.add("Ankita");
+    System.out.println(list);
+		
+	}
+}
